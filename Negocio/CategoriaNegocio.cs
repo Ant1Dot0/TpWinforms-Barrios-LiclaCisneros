8158bc81d43fa5a9f07012bc708611acd35e56c4
Paola Barrios <pbarrios0795@gmail.com>
2022-09-13 18:56:18 -0300

    5

--- Negocio/CategoriaNegocio.cs
+++ Negocio/CategoriaNegocio.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio;
 
 namespace Negocio
 {
     public class CategoriaNegocio
     {
+        public List<Categoria> listar()
+        {
+
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+
+            try
+            {
+                datos.setearQuery("Select Descripcion as Categoria, id from CATEGORIAS");
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+
+                    aux.descripcion = (string)datos.Lector["Descripcion"];
+                    aux.cod = (int)datos.Lector["id"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+
+            }
+
+
+        }
+
     }
 }
